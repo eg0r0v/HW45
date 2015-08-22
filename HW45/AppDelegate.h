d719f2806f7cf2b0c2c543547f6f2bab619c57cb
Илья Егоров <Shepherd_MacBook@ilya.local>
2015-08-22 12:19:51 +0300

    Initial Commit

---
+++ HW45/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW45
+//
+//  Created by Илья Егоров on 22.08.15.
+//  Copyright © 2015 Илья Егоров. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HW45/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW45
+//
+//  Created by Илья Егоров on 22.08.15.
+//  Copyright © 2015 Илья Егоров. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
